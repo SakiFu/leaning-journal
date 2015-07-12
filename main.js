1b6c345921b189ba29dbd3b613e082f9f7e12711
Saki Fu <sakifu.86@gmail.com>
2015-07-11 20:04:19 -0700

    add twitter

---
+++ main.js
@@ -0,0 +1,15 @@
+$( document ).ready( function() {
+
+    $("#addButton").click( function( event ) {
+        event.preventDefault();
+        create();
+    });
+
+    function create() {
+        $.ajax({
+            url: "/add",
+            type: "POST",
+            dataType: "json",
+            data: { "title": $("#title").val(), "text": $("#text").val() },
+        })
+
