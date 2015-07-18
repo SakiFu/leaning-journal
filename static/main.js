82d093b861d763f7ee5650b690c4a149108d4207
Saki Fu <sakifu.86@gmail.com>
2015-07-18 01:55:46 -0700

    resolve conflict

--- static/main.js
+++ static/main.js
@@ -1,14 +1,37 @@
-$( document ).ready( function() {
+$(document).ready(function(){
+            $("#test").click(function(){
+              $('#test').addClass("red");
+            });
+            });
+// $( document ).ready( function() {
+//     $("#addButton").click(function(event) {
+//         event.preventDefault();
+//         sample();
+//     }
+//     function sample(){$("#addButton").click(function(){
+//                $('#test').addClass('red');
+//             });
+    // function create() {
+    //     $.ajax({
+    //         url: "/add",
+    //         type: "POST",
+    //         dataType: "json",
+    //         data: { "title": $("#title").val()},
+    //     })
+    //     .done(function(response) {
+    //         var $template = "<article class='entry'"+
+    //                           "id='"+response.id+"'>"+
+    //                           "<a href='/detail/"+response.id+"'"+
+    //                           "class='title'>"+
+    //                           "<h3>"+response.title+"</h3></a>"+
+    //                           "<p class='dateline'>"+response.date+"</p>"
+    //         $(".entry").prepend($template);
+    //         $('#title').val('');
+    //     })
 
-    $("#addButton").click( function( event ) {
-        event.preventDefault();
-        create();
-    });
+    //     .fail(function(response) {
+    //         alert("error");
+    //     })
+    };
 
-    function create() {
-        $.ajax({
-            url: "/add",
-            type: "POST",
-            dataType: "json",
-            data: { "title": $("#title").val(), "text": $("#text").val() },
-        })
+
